1bee7025df0e453c924f0697068dc47c694155e4
Camilo Gil Taborda <camilo.giltaborda@globant.com>
2023-08-15 09:53:43 -0500

    adding test for naming repo

--- terraformScripts/main.tf
+++ terraformScripts/main.tf
@@ -13,3 +13,13 @@ resource "aws_ssm_parameter" "foo" {
   type  = "String"
   value = "bar"
 }
+
+module "base_naming" {
+  source    = "git::ssh://git@github.com/BrightMLS/common_modules_terraform.git//bright_naming_conventions?ref=v0.0.4"
+  app_group = var.project_app_group
+  env       = var.environment_devops
+  ledger    = var.project_ledger
+  site      = var.site
+  tier      = var.tier
+  zone      = var.zone
+}
