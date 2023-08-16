d4dc930a2ff58ae237fd09f969156215950e86b9
Camilo Gil Taborda <camilo.giltaborda@globant.com>
2023-08-16 11:02:08 -0500

    updating sns_notifications

--- modules/codepipeline_data/modules/sns_notification/main.tf
+++ modules/codepipeline_data/modules/sns_notification/main.tf
@@ -16,16 +16,16 @@ module "cpl_approval_sns_naming" {
 }
 
 resource "aws_sns_topic" "approval" {
-  count = var.sns_notification_create_approval ? 1 : 0
+  #count = var.sns_notification_create_approval ? 1 : 0
   name = module.cpl_approval_sns_naming.name
   tags = module.cpl_approval_sns_naming.tags
 }
 
-resource "aws_ssm_parameter" "approval_notification_parameter" {
-  count       = var.sns_notification_create_approval ? 1 : 0
-  name        = "/parameter/${var.site}/${var.environment_devops}/${var.datapipeline_name}/sns/approval_notification"
-  type        = "SecureString"
-  value       = aws_sns_topic.approval[0].arn
-  description = "Arn for the notification approval sns"
-  overwrite   = true
-}
+#resource "aws_ssm_parameter" "approval_notification_parameter" {
+#  count       = var.sns_notification_create_approval ? 1 : 0
+#  name        = "/parameter/${var.site}/${var.environment_devops}/${var.datapipeline_name}/sns/approval_notification"
+#  type        = "SecureString"
+#  value       = aws_sns_topic.approval[0].arn
+#  description = "Arn for the notification approval sns"
+#  overwrite   = true
+#}
