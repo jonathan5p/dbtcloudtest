aa1fb76bbb184e8647a126a5f574d1a4caa3f448
Camilo Gil Taborda <camilo.giltaborda@globant.com>
2023-10-06 15:05:15 -0500

    adding variable for concurren tasks

--- terraformScripts/variables.tf
+++ terraformScripts/variables.tf
@@ -166,7 +166,7 @@ variable "ecs_subnets" {
   description = "comma separated values for the subnets to use"
 }
 
-variable "number_concurrent_tasks" {
+variable "concurrent_tasks" {
   type = number
   description = "number of concurrent tasks for alaya sync"
   default = 12
