760a5cb660ad6c658ad94747265ff5b2c111d367
jonathan5p <jonathanpinzon05@gmail.com>
2023-10-11 16:12:31 -0500

    Delete full write mode option from ingest job

--- src/mainprocess/glue/ingestjob/ingestjob.py
+++ src/mainprocess/glue/ingestjob/ingestjob.py
@@ -9,75 +9,94 @@ from pyspark.sql import functions as F
 from delta import DeltaTable
 
 
-def unique_by_merge_key(input_df:DataFrame, merge_key:str, precombine_keys:list):
-    unique_per_id = input_df.orderBy([merge_key,*precombine_keys], ascending=False).dropDuplicates([merge_key])
+def unique_by_merge_key(input_df: DataFrame, merge_key: str, precombine_keys: list):
+    unique_per_id = input_df.orderBy(
+        [merge_key, *precombine_keys], ascending=False
+    ).dropDuplicates([merge_key])
     return unique_per_id
 
+
 def full_load(
-        input_df:DataFrame, 
-        target_path:str, 
-        compression:str="snappy",
-        catalog_db:str="default",
-        catalog_table:str="test_table",
-        partitions:str="",
-        test:bool=False
-        ):
-    
+    input_df: DataFrame,
+    target_path: str,
+    compression: str = "snappy",
+    catalog_db: str = "default",
+    catalog_table: str = "test_table",
+    partitions: str = "",
+    test: bool = False,
+):
     current_ts = F.current_timestamp()
 
     # Delta lake Id generation
-    id_df = input_df.withColumn("dlid", F.expr("uuid()"))\
-                    .withColumn("dlingestionts", current_ts)\
-                    .withColumn("dllastmodificationts", current_ts)
-
+    id_df = (
+        input_df.withColumn("dlid", F.expr("uuid()"))
+        .withColumn("dlingestionts", current_ts)
+        .withColumn("dllastmodificationts", current_ts)
+    )
 
     if partitions not in ["", None]:
-        write_df = id_df.write.mode("overwrite")\
-                              .format("delta")\
-                              .option("path", target_path)\
-                              .partitionBy(partitions.split(","))\
-                              .option("compression", compression)\
-                              .option("overwriteSchema", "true")
+        write_df = (
+            id_df.write.mode("overwrite")
+            .format("delta")
+            .option("path", target_path)
+            .partitionBy(partitions.split(","))
+            .option("compression", compression)
+            .option("overwriteSchema", "true")
+        )
     else:
-        write_df = id_df.write.mode("overwrite")\
-                              .format("delta")\
-                              .option("path", target_path)\
-                              .option("overwriteSchema", "true")\
-                              .option("compression", compression)
-    
+        write_df = (
+            id_df.write.mode("overwrite")
+            .format("delta")
+            .option("path", target_path)
+            .option("overwriteSchema", "true")
+            .option("compression", compression)
+        )
+
     if test:
         write_df.save()
     else:
         write_df.saveAsTable(f"{catalog_db}.{catalog_table}")
-        spark.sql(f"ALTER TABLE {catalog_db}.{catalog_table} SET TBLPROPERTIES (delta.enableChangeDataFeed = true)")
+        spark.sql(
+            f"ALTER TABLE {catalog_db}.{catalog_table} SET TBLPROPERTIES (delta.enableChangeDataFeed = true)"
+        )
+
 
 def incremental_load(
-        spark:SparkSession,
-        input_df:DataFrame,
-        target_path:str,
-        merge_key:str,
-        update_condition:str
-        ):
-
+    spark: SparkSession,
+    input_df: DataFrame,
+    target_path: str,
+    merge_key: str,
+    update_condition: str,
+):
     input_df.createOrReplaceTempView("changes_df")
-    target_df = DeltaTable.forPath(spark,target_path)
+    target_df = DeltaTable.forPath(spark, target_path)
     target_df.toDF().createOrReplaceTempView("target_df")
 
-    changes_df = spark.sql(f"select changes_df.*, target_df.dlid, target_df.dlingestionts, target_df.dllastmodificationts from changes_df left join target_df on changes_df.{merge_key}=target_df.{merge_key}")
-    
-    current_ts = F.current_timestamp()
-    changes_with_gid = changes_df.withColumn("dlingestionts", F.when(F.col("dlid").isNull(),
-                                            current_ts).otherwise(F.col("dlingestionts")))\
-                                .withColumn("dlid", F.when(F.col("dlid").isNull(),
-                                            F.expr("uuid()")).otherwise(F.col("dlid")))\
-                                .withColumn("dllastmodificationts", current_ts)
+    changes_df = spark.sql(
+        f"select changes_df.*, target_df.dlid, target_df.dlingestionts, target_df.dllastmodificationts from changes_df left join target_df on changes_df.{merge_key}=target_df.{merge_key}"
+    )
 
-    target_df.alias("target")\
-        .merge(changes_with_gid.alias("source"),f"target.{merge_key} = source.{merge_key}") \
-        .whenMatchedUpdateAll(condition = update_condition)\
-        .whenNotMatchedInsertAll()\
-        .whenNotMatchedBySourceDelete()\
-        .execute()
+    current_ts = F.current_timestamp()
+    changes_with_gid = (
+        changes_df.withColumn(
+            "dlingestionts",
+            F.when(F.col("dlid").isNull(), current_ts).otherwise(
+                F.col("dlingestionts")
+            ),
+        )
+        .withColumn(
+            "dlid",
+            F.when(F.col("dlid").isNull(), F.expr("uuid()")).otherwise(F.col("dlid")),
+        )
+        .withColumn("dllastmodificationts", current_ts)
+    )
+
+    target_df.alias("target").merge(
+        changes_with_gid.alias("source"), f"target.{merge_key} = source.{merge_key}"
+    ).whenMatchedUpdateAll(
+        condition=update_condition
+    ).whenNotMatchedInsertAll().whenNotMatchedBySourceDelete().execute()
+
 
 if __name__ == "__main__":
     args = getResolvedOptions(
@@ -89,7 +108,7 @@ if __name__ == "__main__":
             "catalog_table",
             "catalog_database",
             "options",
-            "connection_type"
+            "connection_type",
         ],
     )
 
@@ -97,11 +116,13 @@ if __name__ == "__main__":
     glueContext = GlueContext(sc)
     spark = glueContext.spark_session
     job = Job(glueContext)
-    job.init(f"{args['JOB_NAME']}-{args['catalog_database']}-{args['catalog_table']}", args)
+    job.init(
+        f"{args['JOB_NAME']}-{args['catalog_database']}-{args['catalog_table']}", args
+    )
 
-    write_options = json.loads(args["options"]).get("write_options",{})
-    conn_ops = json.loads(args["options"]).get("connection_options",{})
-    merge_key=write_options.get("merge_key")
+    write_options = json.loads(args["options"]).get("write_options", {})
+    conn_ops = json.loads(args["options"]).get("connection_options", {})
+    merge_key = write_options.get("merge_key")
     s3_target_path = f"s3://{args.get('target')}/{args.get('target_prefixes')}/{args.get('catalog_database')}/{args.get('catalog_table')}/"
 
     input_df = glueContext.create_dynamic_frame_from_options(
@@ -110,38 +131,32 @@ if __name__ == "__main__":
         transformation_ctx=f"{args['catalog_database']}-{args['catalog_table']}",
     ).toDF()
 
-    precombine_keys=write_options.get("precombine_keys","").split(",")
-    unique_per_id = unique_by_merge_key(input_df,merge_key,precombine_keys)
+    precombine_keys = write_options.get("precombine_keys", "").split(",")
+    unique_per_id = unique_by_merge_key(input_df, merge_key, precombine_keys)
 
-    write_mode = write_options.get("write_mode", "full")
     partitions = write_options.get("partitions")
 
-    if write_mode == "full":
-        
-        full_load(unique_per_id,
-                  s3_target_path,
-                  write_options.get("compression"),
-                  args['catalog_database'],
-                  args['catalog_table'],
-                  partitions=partitions
-                )
+    table_exists = spark._jsparkSession.catalog().tableExists(
+        args["catalog_database"], args["catalog_table"]
+    )
 
-    elif write_mode == "incremental":
+    if not table_exists:
+        full_load(
+            unique_per_id,
+            s3_target_path,
+            write_options.get("compression"),
+            args["catalog_database"],
+            args["catalog_table"],
+            partitions=partitions,
+        )
 
-        if spark._jsparkSession.catalog().tableExists(args['catalog_database'], args['catalog_table']):
-            
-            incremental_load(
-                spark,
-                unique_per_id,
-                s3_target_path,
-                merge_key,
-                write_options.get("update_condition")
-            )
-
-        else: 
-            raise ValueError("Table doesn't exist or Glue data Catalog is not connected. Please run a full load and verify the connection to the Glue data Catalog.")
-
-    else:
-        raise ValueError(f"Incorrect write mode, expected values are [full,incremental], got: {write_mode}")
+    elif table_exists:
+        incremental_load(
+            spark,
+            unique_per_id,
+            s3_target_path,
+            merge_key,
+            write_options.get("update_condition"),
+        )
 
     job.commit()
