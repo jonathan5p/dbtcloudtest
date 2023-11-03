e1eea2688820842c182095b9039e412723ec85a6
Camilo Gil Taborda <camilo.giltaborda@globant.com>
2023-11-03 10:24:28 -0500

    adding permissions

--- src/alayasync/lambda/alayasyncecsstart/lambda_function.py
+++ src/alayasync/lambda/alayasyncecsstart/lambda_function.py
@@ -8,11 +8,26 @@ logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 register_table = dynamodb.Table(os.environ['OIDH_TABLE'])
+async_table = dynamodb.Table(os.environ['STATE_TABLE'])
 
 ecs_client = boto3.client('ecs')
 cluster_name = os.environ['ECS_CLUSTER']
 task_definition = os.environ['TASK_DEFINITION']
 subnet_ids = os.environ['ECS_SUBNETS']
+
+lambda_client = boto3.client('lambda')
+
+def put_item(request_id, state, payload):
+    response = async_table.put_item(
+        Item={
+            'request_id': request_id,
+            'state': state,
+            'payload': payload
+        },
+        ConditionExpression='attribute_not_exists(request_id)'
+    )
+
+    return response
 
 def parse_event(event):
     
@@ -22,38 +37,11 @@ def parse_event(event):
         task_args.append(event[item])
         
     logger.info(f'Parsed args: {task_args}')
-    
-    task_args = add_primary_key(task_args, event['table'])
     return task_args
     
-def add_primary_key(task_args, table):
-    
-    task_args.append('--primary_key')
-    task_args.append(os.environ[table.upper()])
-    
-    logger.info(f'Primary key: {os.environ[table.upper()]}')
-    return task_args
-
-def lambda_handler(event, context):
-    
-    logger.info(f'Event received: {event}')
+def run_ecs(event):
     
     task_arguments = parse_event(event['value'])
-    
-    ids = event['value']['id'].split(',')
-    
-    logger.info(f'Updating ids:{ids}')
-    
-    for id in ids:
-    
-        response = register_table.update_item(
-            Key={'id': id},
-            UpdateExpression="set #p=:p, #e=:e, #t=:t, #r=:r",
-            ExpressionAttributeValues={':p': 'IN_PROGRESS', ':e': '', ':t':'', ':r':{}},
-            ExpressionAttributeNames={"#p": "status", "#e": "error", "#t":"task_id", "#r":"records"},
-            ReturnValues="UPDATED_NEW")
-
-        #logger.info(f'Response from updates: {response}')
     
     response = ecs_client.run_task(
         cluster = cluster_name,
@@ -79,19 +67,63 @@ def lambda_handler(event, context):
 
     task_arn = response['tasks'][0]['taskArn']
     task_id = task_arn.split("/")[-1]
+    
+    return task_id
+    
+def run_lambda(event):
+    
+    FUNCTION_NAME = 'aue1d1z1lmboidhoidh-alayasyncasync'
+    
+    task_arguments = event['value']
+    
+    response = lambda_client.invoke(
+        FunctionName=FUNCTION_NAME,
+        InvocationType='Event',
+        Payload=json.dumps(task_arguments))
+        
+    request_id = response.get('ResponseMetadata', {}).get('RequestId')
+    
+    if response.get('StatusCode') in [200, 202] and request_id:
+        response = put_item(request_id, 'STARTED', task_arguments)
+        logger.info(f'Function {FUNCTION_NAME} started with RequestId: {request_id}')
+    else:
+        logger.error(f'Something failed when calling {FUNCTION_NAME}. Check {response}.')
+        raise ValueError('Lambda call failed')
 
+    return request_id
+    
+
+def lambda_handler(event, context):
+    
+    logger.info(f'Event received: {event}')
+    processing_engine = 'lambda_function'
+    
+    id = event['value']['id']
+    response = register_table.update_item(
+        Key={'id': id},
+        UpdateExpression="set #p=:p, #e=:e, #t=:t, #pe=:pe",
+        ExpressionAttributeValues={':p': 'IN_PROGRESS', ':e': '', ':t':'', ':pe': processing_engine},
+        ExpressionAttributeNames={"#p": "status", "#e": "error", "#t":"task_id", "#pe": "processing_engine"},
+        ReturnValues="UPDATED_NEW")
+
+    logger.info(f'Response from updates: {response}')
+    
+    if processing_engine == 'ecs':
+        task_id = run_ecs(event)
+    elif processing_engine == 'lambda_function':
+        task_id = run_lambda(event)
+    
     logger.info(f'Task initiatted with task_id: {task_id}')
 
-    for id in ids:
-    
-        response = register_table.update_item(
-            Key={'id': id},
-            UpdateExpression="set #t=:t",
-            ExpressionAttributeValues={':t': task_id},
-            ExpressionAttributeNames={"#t": "task_id"},
-            ReturnValues="UPDATED_NEW")
+    response = register_table.update_item(
+        Key={'id': id},
+        UpdateExpression="set #t=:t, #pe=:pe",
+        ExpressionAttributeValues={':t': task_id, ':pe': processing_engine},
+        ExpressionAttributeNames={"#t": "task_id", "#pe": "processing_engine"},
+        ReturnValues="UPDATED_NEW")
 
     return {
         'statusCode': 200,
-        'task_id': task_id
+        'task_id': task_id,
+        'processing_engine': processing_engine
     }
--- src/alayasync/lambda/alayasyncecsstatus/lambda_function.py
+++ src/alayasync/lambda/alayasyncecsstatus/lambda_function.py
@@ -6,13 +6,33 @@ import os
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+cluster_name = os.environ['ECS_CLUSTER']
 ecs_client = boto3.client('ecs')
 
-cluster_name = os.environ['ECS_CLUSTER']
+dynamodb = boto3.resource('dynamodb')
+async_table = dynamodb.Table(os.environ['STATE_TABLE'])
 
-def lambda_handler(event, context):
+projection_expression = "#i, #s"
+expression_attributes_names = {"#i": "request_id", "#s": "state"}
+
+query_parameters = {
+        'ProjectionExpression': projection_expression,
+        'ExpressionAttributeNames': expression_attributes_names
+    }
+
+start_timeout_retries = 10
     
-    logger.info(f"Event received: {event}")
+def get_from_dynamo(table, query_parameters):
+
+    response = table.get_item(**query_parameters)
+    logger.info(f'Response:{response}')
+    
+    validate_response(response)
+    records = response.get('Item')
+
+    return records
+
+def status_ecs(event):
     
     task_id = event['run_async_id']['task_id']
     
@@ -40,10 +60,70 @@ def lambda_handler(event, context):
     
     if stop_code not in ['', 'EssentialContainerExited']:
         raise ValueError(f'Container failed with StopCode: {stop_code}')
+        
+    return status, task_status, exit_code, False
+    
+def status_lambda(event):
+    
+    task_id = event['run_async_id']['task_id']
+    
+    query_parameters['Key'] = {'request_id': task_id}
+    record = get_from_dynamo(async_table, query_parameters)
+    
+    logger.info(f'Records: {record}')
+    
+    status = record['state']
+    #status = 'STOPPED'
+    task_status = event.get('run_async_status',{'task_status': []})['task_status']
+    start_timeout = event.get('run_async_status',{'start_timeout': True})['start_timeout']
+    
+    logger.info(f'Task Status: {task_status}')
+    logger.info(f'Status: {status}')
+    logger.info(f'Start Timeout: {start_timeout}')
+    
+    if start_timeout:
+        if (status == 'STARTED') and (len(task_status) == start_timeout_retries):
+            raise ValueError(f'Start Timeout for the lambda function:{task_id}')
+        elif (status != 'STARTED'):
+            start_timeout = False
+    
+    task_status.append(status)
+    logger.info(f'After Status: {task_status}')
+    exit_code = 0
+    
+    return status, task_status, exit_code, start_timeout
+    
+    
+def validate_response(response):
+
+    try:
+        status = response.get('ResponseMetadata', {}).get('HTTPStatusCode')
+
+        if status == 200:
+            return None
+
+    except Exception as e:
+        logger.error(f'Error checking Dynamo response: {repr(e)}')
+        raise e
+        
+    return None
+    
+def lambda_handler(event, context):
+    
+    logger.info(f"Event received: {event}")
+    
+    processing_engine = event['run_async_id']['processing_engine']
+    
+    if processing_engine == 'ecs':
+        status, task_status, exit_code, start_timeout = status_ecs(event)
+    elif processing_engine == 'lambda_function':
+        status, task_status, exit_code, start_timeout = status_lambda(event)
+    
     
     return {
+        'exit_code': exit_code,
         'statusCode': 200,
         'status': status,
-        'task_status': task_status,
-        'exit_code': exit_code
+        'start_timeout': start_timeout,
+        'task_status': task_status
     }
