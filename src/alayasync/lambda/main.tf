7c9f04ec2a8cee4285b2b65b3dc4b1ce0d1c4dac
Camilo Gil Taborda <camilo.giltaborda@globant.com>
2023-11-14 16:11:08 -0500

    deleting unused lambda

--- src/alayasync/lambda/main.tf
+++ src/alayasync/lambda/main.tf
@@ -49,25 +49,25 @@ module "lambda_alaya_sync_scheduling" {
   ]
 }
 
-module "lambda_alaya_sync_processing" {
-  source = "git::ssh://git@github.com/BrightMLS/bdmp-terraform-pipeline.git//lambdas?ref=v0.0.4"
+#module "lambda_alaya_sync_processing" {
+#  source = "git::ssh://git@github.com/BrightMLS/bdmp-terraform-pipeline.git//lambdas?ref=v0.0.4"
 
-  environment       = var.environment
-  lambda_name       = "alayasyncprocessing"
-  lambda_path       = "../src/alayasync/lambda"
-  project_app_group = var.project_app_group
-  project_ledger    = var.project_ledger
-  project_prefix    = var.project_prefix
-  site              = var.site
-  tier              = var.tier
-  zone              = var.zone
+#  environment       = var.environment
+#  lambda_name       = "alayasyncprocessing"
+#  lambda_path       = "../src/alayasync/lambda"
+#  project_app_group = var.project_app_group
+#  project_ledger    = var.project_ledger
+#  project_prefix    = var.project_prefix
+#  site              = var.site
+#  tier              = var.tier
+#  zone              = var.zone
 
-  policy_variables = var.project_objects
+#  policy_variables = var.project_objects
 
-  environment_variables = {
-    "OIDH_TABLE" = var.project_objects.dynamo_table_register
-  }
-}
+#  environment_variables = {
+#    "OIDH_TABLE" = var.project_objects.dynamo_table_register
+#  }
+#}
 
 module "lambda_alaya_sync_reduce" {
   source = "git::ssh://git@github.com/BrightMLS/bdmp-terraform-pipeline.git//lambdas?ref=v0.0.4"
